7a2cbab9ad4248746999a089b445da082650313b
teraja <teraja@cisco.com>
2020-01-07 16:21:23 +0530

    added in-build and custom validator

--- models/customers.js
+++ models/customers.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const Joi = require('@hapi/joi');
 
+//For practice we have implement the custom validator and in-built validator, else it is not required.
 const customerSchema = new mongoose.Schema({
     isGold: {
         type: Boolean,
@@ -10,12 +11,18 @@ const customerSchema = new mongoose.Schema({
         type: String,
         minlength: 3,
         maxlength: 30,
-        required: true
+        required: function() {
+            return this.isGold;
+        }
     },
     phone: {
         type: String,
-        minlength: 3,
-        maxlength: 30,
+        validate : {
+            validator: function(v) {
+                return v.length === 10;
+            },
+            message :"phone number must be 10 numbers"
+        },
         required: true
     }
 });
@@ -25,7 +32,7 @@ const Customer = mongoose.model('customer', customerSchema, 'customers');
 function validate( body ) {
     const schema = Joi.object({
         isGold: Joi.boolean().required(),
-        name: Joi.string().min(3).max(30).required(),
+        name: Joi.string().min(3).max(30).optional(),
         phone: Joi.string().min(3).max(30).optional()
     });
 
